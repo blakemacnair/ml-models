29615862e3be50ba592df7d2c771be525b812577
Blake Macnair <blakemacnair@gmail.com>
2020-01-30 18:36:16 -0600

    Add some epochs to try overfitting on repeat data.

--- titanic/titanic-classification-neural.py
+++ titanic/titanic-classification-neural.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     X_test = Tensor(X_test)
 
-    cv = ShuffleSplit(n_splits=3, test_size=0.4, random_state=0)
+    cv = ShuffleSplit(n_splits=3, test_size=0.3, random_state=0)
     epoch = 0
     for train_index, test_index in cv.split(X, y):
         mbot = MurderBot(input_dim=X.shape[1],
@@ -37,9 +37,15 @@ if __name__ == "__main__":
         test_y = y[test_index]
 
         train_dataset = TensorDataset(train_x, train_y)
-        train_loader = DataLoader(dataset=train_dataset, batch_size=5)
+        train_loader = DataLoader(dataset=train_dataset, batch_size=10)
 
-        loss, acc = mbot.fit(train_loader, criterion, optimizer)
+        loss = []
+        acc = []
+        for epoch in range(3):
+            loss_epoch, acc_epoch = mbot.fit(train_loader, criterion, optimizer)
+            loss += loss_epoch
+            acc += acc_epoch
+
         fig, (ax1, ax2) = plt.subplots(1, 2)
         ax1.set_title("Loss")
         ax1.plot(loss)
